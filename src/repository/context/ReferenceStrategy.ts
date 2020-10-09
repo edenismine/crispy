6401a12e0199a43a6198d2a8308f676804d3197e
Daniel Aragon <luis.aragon@globant.com>
2020-10-09 14:36:32 -0500

    fix(referencestrategy): throw ValueError on empty id, parentCollectionName or parentId

--- src/repository/context/ReferenceStrategy.ts
+++ src/repository/context/ReferenceStrategy.ts
@@ -1,5 +1,6 @@
 import { CollectionReference, DocumentReference } from '@google-cloud/firestore'
 import { NestedId } from '../../common'
+import { RepositoryError } from '../errors'
 
 export type ReferenceStrategy<ID = string> = (
   collection: CollectionReference,
@@ -14,7 +15,33 @@ type Defaults = {
 }
 
 export const ReferenceStrategy: Defaults = {
-  Collection: (collection, id) => collection.doc(id),
-  NestedCollection: (nestedCollectionName) => (collection, { id, parentId }) =>
-    collection.doc(parentId).collection(nestedCollectionName).doc(id),
+  Collection: (collection, id) => {
+    if (!id)
+      throw RepositoryError.of(
+        'ValueError',
+        'Value for argument "id" is not a valid resource path. "id" must be a non-empty string',
+      )
+    return collection.doc(id)
+  },
+  NestedCollection: (nestedCollectionName) => (
+    collection,
+    { id, parentId },
+  ) => {
+    if (!id)
+      throw RepositoryError.of(
+        'ValueError',
+        'Value for argument "id" is not a valid resource path. "id" must be a non-empty string',
+      )
+    if (!nestedCollectionName)
+      throw RepositoryError.of(
+        'ValueError',
+        'Value for argument "nestedCollectionName" is not a valid resource path. "nestedCollectionName" must be a non-empty string',
+      )
+    if (!parentId)
+      throw RepositoryError.of(
+        'ValueError',
+        'Value for argument "parentId" is not a valid resource path. "parentId" must be a non-empty string',
+      )
+    return collection.doc(parentId).collection(nestedCollectionName).doc(id)
+  },
 }
